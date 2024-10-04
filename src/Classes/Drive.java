089946b7486458432f67bdc2a95784f59bbd8216
Maria <mcuervo@correo.unimet.edu.ve>
2024-10-03 23:42:33 -0400

    Clase Drive

--- src/Classes/Drive.java
+++ src/Classes/Drive.java
@@ -11,13 +11,39 @@ import java.util.concurrent.Semaphore;
  * @author Maria Daniela
  */
 public class Drive {
-    public Developer(int rol, String name, int dayDuration, int payPerHour, Semaphore mutex, Drive drive) {
-        this.rol = rol;
-        this.name = name;
-        this.dayDuration = dayDuration;
-        this.payPerHour = payPerHour;
-        this.mutex = mutex;
-        this.drive = drive;
+    private int placasbases;
+    private int cpus; 
+    private int memoriaram; 
+    private int fuentealim;
+    private int tarjgraficas; 
+    
+    private int maxplacasbases;
+    private int maxcpus; 
+    private int maxmemoriaram; 
+    private int maxfuentealim;
+    private int maxtarjgraficas; 
+    
+    private int computadoras; 
+    private int computadorasytgraficas; 
+
+    public Drive(int maxplacasbases, int maxcpus, int maxmemoriaram, int maxfuentealim, int maxtarjgraficas, int computadoras, int computadorasytgraficas) {
+        this.placasbases = 0;
+        this.cpus = 0;
+        this.memoriaram = 0;
+        this.fuentealim = 0;
+        this.tarjgraficas = 0;
+        this.maxplacasbases = maxplacasbases;
+        this.maxcpus = maxcpus;
+        this.maxmemoriaram = maxmemoriaram;
+        this.maxfuentealim = maxfuentealim;
+        this.maxtarjgraficas = maxtarjgraficas;
+        this.computadoras = computadoras;
+        this.computadorasytgraficas = computadorasytgraficas;
     }
+    
+    public void Limites(){
+    }
+    
+    public void AggDrive (int rol, int content )
     
 }
