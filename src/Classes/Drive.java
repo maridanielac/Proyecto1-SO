51408ceb3c14b3e5450c19aae356e69489b83740
Maria <mcuervo@correo.unimet.edu.ve>
2024-10-04 00:08:21 -0400

    Interfaz

--- src/Classes/Drive.java
+++ src/Classes/Drive.java
@@ -44,6 +44,135 @@ public class Drive {
     public void Limites(){
     }
     
-    public void AggDrive (int rol, int content )
+    public void AggDrive (int rol, int content ){
+        switch (rol){
+            case 0: { if (getPlacasbases()+content > getMaxplacasbases()){
+                setPlacasbases(getPlacasbases());
+            }else {
+                setPlacasbases(getPlacasbases()+content);
+            }}
+            case 1: { if (getCpus()+content > getMaxcpus()){
+                setCpus(getCpus());
+            }else {
+                setCpus(getCpus()+content);
+            }}
+            case 2: { if (getMemoriaram()+content > getMaxmemoriaram()){
+                setMemoriaram(getMemoriaram());
+            }else {
+                setMemoriaram(getMemoriaram()+content);
+            }}
+            case 3: { if (getFuentealim()+content > getMaxfuentealim()){
+                setFuentealim(getFuentealim());
+            }else {
+                setFuentealim(getFuentealim()+content);
+            }}
+            case 4: { if (getTarjgraficas()+content > getMaxtarjgraficas()){
+                setTarjgraficas(getTarjgraficas());
+            }else {
+                setTarjgraficas(getTarjgraficas()+content);
+            }}
+            
+            default:
+                System.out.println("No existen mas empleados");
+        }
+    }
+
+    public int getPlacasbases() {
+        return placasbases;
+    }
+
+    public void setPlacasbases(int placasbases) {
+        this.placasbases = placasbases;
+    }
+
+    public int getCpus() {
+        return cpus;
+    }
+
+    public void setCpus(int cpus) {
+        this.cpus = cpus;
+    }
+
+    public int getMemoriaram() {
+        return memoriaram;
+    }
+
+    public void setMemoriaram(int memoriaram) {
+        this.memoriaram = memoriaram;
+    }
+
+    public int getFuentealim() {
+        return fuentealim;
+    }
+
+    public void setFuentealim(int fuentealim) {
+        this.fuentealim = fuentealim;
+    }
+
+    public int getTarjgraficas() {
+        return tarjgraficas;
+    }
+
+    public void setTarjgraficas(int tarjgraficas) {
+        this.tarjgraficas = tarjgraficas;
+    }
+
+    public int getMaxplacasbases() {
+        return maxplacasbases;
+    }
+
+    public void setMaxplacasbases(int maxplacasbases) {
+        this.maxplacasbases = maxplacasbases;
+    }
+
+    public int getMaxcpus() {
+        return maxcpus;
+    }
+
+    public void setMaxcpus(int maxcpus) {
+        this.maxcpus = maxcpus;
+    }
+
+    public int getMaxmemoriaram() {
+        return maxmemoriaram;
+    }
+
+    public void setMaxmemoriaram(int maxmemoriaram) {
+        this.maxmemoriaram = maxmemoriaram;
+    }
+
+    public int getMaxfuentealim() {
+        return maxfuentealim;
+    }
+
+    public void setMaxfuentealim(int maxfuentealim) {
+        this.maxfuentealim = maxfuentealim;
+    }
+
+    public int getMaxtarjgraficas() {
+        return maxtarjgraficas;
+    }
+
+    public void setMaxtarjgraficas(int maxtarjgraficas) {
+        this.maxtarjgraficas = maxtarjgraficas;
+    }
+
+    public int getComputadoras() {
+        return computadoras;
+    }
+
+    public void setComputadoras(int computadoras) {
+        this.computadoras = computadoras;
+    }
+
+    public int getComputadorasytgraficas() {
+        return computadorasytgraficas;
+    }
+
+    public void setComputadorasytgraficas(int computadorasytgraficas) {
+        this.computadorasytgraficas = computadorasytgraficas;
+    }
+    
+    
     
 }
--- src/proyecto1/so/Proyecto1SO.java
+++ src/proyecto1/so/Proyecto1SO.java
@@ -19,19 +19,19 @@ public class Proyecto1SO {
     public static void main(String[] args) {
         // TODO code application logic 
         
-        Semaphore mainMutex = new Semaphore(1);
+        //Semaphore mainMutex = new Semaphore(1);
         
-        Developer dev1 = new Developer(1,20, mainMutex);
-        Developer dev2 = new Developer(2,26, mainMutex);
-        Developer dev3 = new Developer(3,40, mainMutex);
-        Developer dev4 = new Developer(4,16, mainMutex);
-        Developer dev5 = new Developer(5,14, mainMutex);
+        //Developer dev1 = new Developer(1,20, mainMutex);
+        //Developer dev2 = new Developer(2,26, mainMutex);
+        //Developer dev3 = new Developer(3,40, mainMutex);
+        //Developer dev4 = new Developer(4,16, mainMutex);
+        //Developer dev5 = new Developer(5,14, mainMutex);
         
-        dev1.start();
-        dev2.start();
-        dev3.start();
-        dev4.start();
-        dev5.start();
+        //dev1.start();
+        //dev2.start();
+        //dev3.start();
+        //dev4.start();
+        //dev5.start();
         
     }
     
