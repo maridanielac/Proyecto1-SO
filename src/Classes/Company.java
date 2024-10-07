8657a29dcfdbc730bb04aec87e13b50ea26806a3
Maria <mcuervo@correo.unimet.edu.ve>
2024-10-07 01:46:21 -0400

    News

--- src/Classes/Company.java
+++ src/Classes/Company.java
@@ -4,6 +4,8 @@
  */
 package Classes;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author Maria Daniela
@@ -21,6 +23,57 @@ public class Company {
     
     private float computers; 
     private float computerstarjetasgraficas; 
+    
+    private int PlacasBasesSalary; 
+    private int CpusSalary; 
+    private int MemoriaRamSalary; 
+    private int FuenteAliSalary;
+    private int TarjetasGraficasSalary; 
+    private int assemblerSalary;
+    
+    private int dayDuration; 
+    
+    private int PlacasBasesContent; 
+    private int CpusContent; 
+    private int MemoriaRamContent; 
+    private int FuenteAliContent; 
+    private int TarjetaGraficaContent; 
+    
+    private int placasbasesToWork; 
+    private int cpusToWork; 
+    private int memoriaramToWork;
+    private int fuentealiToWork; 
+    private int tarjetagraficaToWork; 
+    private int assemblerToWork; 
+    
+    private int placasbasesEnsamblar; 
+    private int cpusEnsamblar; 
+    private int memoriaramEnsamblar;
+    private int fuentealiEnsamblar; 
+    private int tarjetagraficaEnsamblar; 
+    private int assemblerEnsamblar; 
+    
+    private int compusMemoria; 
+    private Semaphore mutex; 
+    private int fixComitDay; 
+    private int comitDay; 
+    private Drive drive; 
+    
+    private int PMsalary; 
+    private int directorSalary; 
+    
+    private Developer ProductosBases; 
+    private Developer Cpus; 
+    private Developer MemoriaRam;
+    private Developer FuenteAli; 
+    private Developer TarjetaGrafica; 
+    private Assembler Assembler; 
+    
+    private int trabajadoresTotalesMax; 
+    private int trabajadoresTotales; 
+    private int guardarTrabajadoresTotales;
+    
+    
     
     
 }
